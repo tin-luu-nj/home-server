6cd005a565bb9e3f9ab4a3f2e72d9af50a5d7c8f
Tin T. Luu <tin.luu.nj@gmail.com>
2023-10-21 19:07:40 +0700

    #1 Diagnostic: set, look-up and dump a status event
    Changes:
    + Static Analysis

---
+++ static.bat
@@ -0,0 +1,4 @@
+radon cc .\src\ .\test\ .\Launcher.py > static_analysis\cc.txt
+radon raw .\src\ .\test\ .\Launcher.py > static_analysis\raw.txt
+radon mi .\src\ .\test\ .\Launcher.py > static_analysis\mi.txt
+radon hal .\src\ .\test\ .\Launcher.py > static_analysis\hal.txt
